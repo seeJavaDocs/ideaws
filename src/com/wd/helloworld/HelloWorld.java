69193cf79fac3274ebc82845e3afb856b156b279
wudong_glb <wudong308_glb@qq.com>
2019-04-21 15:34:04 +0800

    测试commit,push

--- src/com/wd/helloworld/HelloWorld.java
+++ src/com/wd/helloworld/HelloWorld.java
@@ -6,21 +6,6 @@ import java.util.List;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("");
-    }
-
-
-    public HelloWorld() {
-
-    }
-
-
-
-    /**
-     *
-     */
-    public void show() {
-        List<String> list = new ArrayList<String>();
-
+        System.out.println("hello github ...");
     }
 }
